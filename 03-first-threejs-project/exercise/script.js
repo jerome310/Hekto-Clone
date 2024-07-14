08915211049baeb5b2ca22f9373bb2a87534ab73
jerome310 <dreammedia365@yahoo.com>
2024-07-14 03:22:23 -0400

    random stuff

---
+++ 03-first-threejs-project/exercise/script.js
@@ -0,0 +1,39 @@
+// Notes: Mesh is a combination of geometry and material
+// Geometry: The shape of the object
+// Material: How it looks
+
+import * as THREE from 'three';
+
+// Canvas
+const canvas = document.querySelector('canvas.webgl');
+
+// Scene
+const scene = new THREE.Scene();
+
+// Mesh Object
+const geometry = new THREE.BoxGeometry(1, 1, 1);
+const material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
+const mesh = new THREE.Mesh(geometry, material);
+
+// Adding Mesh to the Scene
+scene.add(mesh)
+
+// Sizes
+const sizes = {
+    width: 800,
+    height: 600
+}
+
+// Camera
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height);
+camera.position.z = 3
+scene.add(camera)
+
+// Renderer
+const renderer = new THREE.WebGLRenderer({
+    canvas: canvas
+});
+
+renderer.setSize(sizes.width, sizes.height )
+
+renderer.render(scene, camera)
